b821b463df01eb7872eff6c46ee4feddec2ed4ba
AeRiaSaito <florence.navet@laplateforme.io>
2024-11-25 13:08:16 +0100

    envoi job 1 à 3

---
+++ main.py
@@ -0,0 +1,11 @@
+#programme affichant chif de 0 à 20
+for number in range (21):
+    print(number)
+#programme affichant chif de 0 à 20 - chif 1 sur 2
+for i in range (21):
+    if i % 2 == 0:
+        print(i)
+#programme affichat chif 1 à 20 - affichant le carré
+for i in range(1, 21):
+    print(f"Le carré de {i} est : {i**2}")
+    
